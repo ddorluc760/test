118ae7e2176c10dadd91de5ba95f7452d07eb04e
David <ddorluc760@iesalmudeyne.es>
2023-11-20 16:51:24 +0100

    Version HolaMundo.java con colores

--- HolaMundo.java
+++ HolaMundo.java
@@ -1,5 +1,7 @@
 public class HolaMundo{
         public static void main (String[] args){
+            String rojo = "\033[32m";
+            String azul = "\033[33m";
             System.out.println("Hola Mundo");
             System.out.pritnln("Aquí estamos haciendo pruebas en Git.")
         }
