01ad1d32d84ae21e8026b6403f7f84f74b548a5a
David <ddorluc760@iesalmudeyne.es>
2023-11-20 16:47:25 +0100

    Versión inicial del fichero HolaMundo.java sin colores

---
+++ HolaMundo.java
@@ -0,0 +1,6 @@
+public class HolaMundo{
+        public static void main (String[] args){
+            System.out.println("Hola Mundo");
+            System.out.pritnln("Aquí estamos haciendo pruebas en Git.")
+        }
+}
